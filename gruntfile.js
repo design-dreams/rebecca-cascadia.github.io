dad84f5b5bb9e00f955887007d69a531559a779e
Rebecca <rmiller@Rebeccas-MacBook-Air.local>
2018-05-14 21:40:02 -0700

    added all of the grunt plugins and ran them

--- gruntfile.js
+++ gruntfile.js
@@ -10,29 +10,44 @@ module.exports = function(grunt) {
                     style: 'expanded'
                 },
                 files: {                       
-                'css/nav.css': 'assets/scss/nav.scss',   // 'destination': 'source'
+                'css/nav.css': 'assets/scss/nav.scss'   // 'destination': 'source'
                 }
             }
-        }
-        watch: {
-            scripts: {
-                files: '**/*.js',
-                tasks: ['generateFileManifest'],
+        },
+        uglify: {
+            my_target: {
+                files: {
+                'js/animate.min.js': ['assets/js/animate.js']
+                }
+            }
+        },
+        watch: {      
+            configFiles: {
+                files: [ 'assets/scss/nav.scss' ],
+                tasks: ['sass'],   //runs sass task from above
                 options: {
-                    event: ['all'],
-                    interrupt: true,
+                  event: ['all'],
+                  reload: true
                 }
             }
+        },
+        jshint: {
+            all: ['assets/js/*.js']
         }
+        
     });
 
 	//Loading Grunt plugins and tasks
 	require('load-grunt-tasks')(grunt);
 
 	//Custom tasks
-	grunt.registerTask('default', ['sass','watch']);
-    
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('default', ['sass','uglify','jshint','watch']);
+    //watch needs to be last, runs in order
+    
+    
 
 };
